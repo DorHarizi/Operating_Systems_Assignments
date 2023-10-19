6e3b5c5b12b830d39476daba62ba9e0219d89f4e
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2023-10-19 19:01:49 +0300

    Update codecB.c

--- Task1_ShellOps/codecB.c
+++ Task1_ShellOps/codecB.c
@@ -1,15 +1,17 @@
-
+// Function to encode a string using codecB
 void encode_codecB(char* str){
     int i = 0;
     while(str[i]){
-        str[i] = str[i] + 3;
-        i++;
+        str[i] = str[i] + 3;  // Shift each character 3 places forward in the ASCII table
+        i++;  // Move on to the next character in the string
     }
 }
+
+// Function to decode a string using codecB
 void decode_codecB(char* str){
     int i = 0;
     while(str[i]){
-        str[i] = str[i] - 3;
-        i++;
+        str[i] = str[i] - 3;  // Shift each character 3 places backward in the ASCII table to decode
+        i++;  // Move on to the next character in the string
     }
 }
