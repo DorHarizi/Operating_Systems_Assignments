fe4d8491d2cdb880d69e30a324baba9f47fc1a31
DorHarizi <93187894+DorHarizi@users.noreply.github.com>
2023-10-19 19:00:23 +0300

    Update codecA.c

--- Task1_ShellOps/codecA.c
+++ Task1_ShellOps/codecA.c
@@ -1,20 +1,25 @@
 #include <ctype.h>
 
+// Function to encode a string using codecA
 void encode_codecA(char* str){
     int i = 0;
     while (str[i])
     {
+        // If the character is lowercase, convert it to uppercase
         if(islower(str[i])){
             str[i] = toupper(str[i]);
         }else{
+            // If the character is uppercase, convert it to lowercase
             if(isupper(str[i])){
                 str[i] = tolower(str[i]);
             }
         }
-        i++;
+        i++;  // Move on to the next character in the string
     }
-    
 }
+
+// Function to decode a string using codecA
+// Since encoding and decoding are the same for codecA, we just call the encode function
 void decode_codecA(char* str){
-    encode_codecA(str);
+    encode_codecA(str);  // Call the encode function to decode, as the process is the same
 }
